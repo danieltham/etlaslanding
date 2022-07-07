a4a43b2a1b6ff9eb5af14f87eb0b73b5ffad6f75
Fabian Koh <FabianKoh@fabiankoh.local>
2022-07-07 17:16:16 +0800

    Fixing features section

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -19,28 +19,16 @@ export default function Features(){
                         <h5>Unified Security Platform</h5>
                         <p>Etlas provides a single web-based portal to access all of your security needs. View and configure access control, alarm systems, cameras, recorders and more, all from a unified platform.</p>
                     </div>
+                </div>
+                <div className="features_box">
                     <div className="feature">
                     <img alt='' src={truckImg} />
                         <h5>Access from Anywhere </h5>
                         <p>Using modern browsers like Chrome, Edge, Mozilla and Safari, access Etlas from anywhere with almost any device. This eliminates the need for installing different applications on your workstations, laptops and mobile devices.</p>
                     </div>
                 </div>
-
                 <div className="feature_img">
                     <img alt='' src={featureImg}/>
-                </div>
-
-                <div className="features_box">
-                    <div className="feature">
-                    <img alt='' src={tvImg} />
-                        <h5>Modern. Intuitive. User-centric</h5>
-                        <p>With user experience as a key focus, we strive to provide the most intuitive and modern user interface in the market. This enables you and your team to get started with minimal onboarding and training.</p>
-                    </div>
-                    <div className="feature">
-                    <img alt='' src={puzzleImg} />
-                        <h5>Modularity </h5>
-                        <p>Etlas allows you to select only the features you require, paying only for what you need. No complex licensing bundles and bloated software. Choose to deploy on your own premises or on  our cloud platforms; the user experience will be completely the same.</p>
-                    </div>
                 </div>
 
                 <div className="features_box">
@@ -50,6 +38,22 @@ export default function Features(){
                         <p>In our design concept, we placed emphasis on flexibility for our users. You may use our control boards for access control, alarm  systems, or for general signaling to secondary systems. A single type of hardware to simplify your implementation while maintaining the ability to cater for complex requirements.</p>
                     </div>
                 </div>
+                <div className="features_box">
+                    <div className="feature">
+                    <img alt='' src={tvImg} />
+                        <h5>Modern. Intuitive. User-centric</h5>
+                        <p>With user experience as a key focus, we strive to provide the most intuitive and modern user interface in the market. This enables you and your team to get started with minimal onboarding and training.</p>
+                    </div>
+                </div>
+                <div className="features_box">
+                    <div className="feature">
+                    <img alt='' src={puzzleImg} />
+                        <h5>Modularity </h5>
+                        <p>Etlas allows you to select only the features you require, paying only for what you need. No complex licensing bundles and bloated software. Choose to deploy on your own premises or on  our cloud platforms; the user experience will be completely the same.</p>
+                    </div>
+                </div>
+                
+
             </div>
         </div>
     )
